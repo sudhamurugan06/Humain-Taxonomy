c3b55395fb6e9ec40c21fd7795cbb741ab6def81
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-09-17 10:58:39 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,11 @@ import re
 import nltk
 import six
 import bs4
+import pandas as pd
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+text=pd.read_csv(r'D:\Users\Sudha\Desktop\datasets\Train_Sample.csv')
+text.head()
 def remove_tags(text):
     tags_to_filter = ['code', 'a']
     if isinstance(text, six.string_types):
@@ -12,23 +15,24 @@ def remove_tags(text):
     for tag_to_filter in tags_to_filter:
         text_to_remove = soup.findAll(tag_to_filter)
         [tag.extract() for tag in text_to_remove]
-    return soup.get_text()
+    print(soup.get_text())
 
 def rem_body_space(text):
     text = re.sub("\'", "", text)
     text = re.sub("[^a-zA-Z]", " ", text)
     text = ' '.join(text.split())
     text = text.lower()
-    return text
+    print( text)
 
 def freq_words(x, terms=30):
     all_words = [text for text in x]
     fdist = nltk.FreqDist(all_words)
     words_df = pd.DataFrame({'word': list(fdist.keys()), 'count': list(fdist.values())})
     d = words_df.nlargest(columns="count", n=terms)
+    print(d)
 
 
 def remove_stopwords(text):
     stop_words = set(stopwords.words('english'))
     no_stopwords_text = [w for w in text.split() if not w in stop_words]
-    return ' '.join(no_stopwords_text)
+    print( ' '.join(no_stopwords_text))
