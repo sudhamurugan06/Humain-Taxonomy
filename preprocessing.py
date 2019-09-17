936bcf437ef941f7bb53dd1faf3380203d396efc
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-09-17 10:50:00 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,72 +1,34 @@
-import sys
-import csv as csv
+import re
+import nltk
+import six
+import bs4
 from bs4 import BeautifulSoup
-import os
-
-def main():
-    stop_w = get_words('words.csv')
-    # if no filenames were given as input it would try to run on the default 'train.csv' and 'test.csv'
-    if len(sys.argv)==1:
-        preprocess_csv('train.csv',stop_w)
-        preprocess_csv('test.csv',stop_w)
-    # pre process any given filename in the command line input
-    for arg in sys.argv[1:]:
-        preprocess_csv(arg,stop_w)
-
-
-def filter_common_words(text, stop_w):
-    if isinstance(text, unicode):
+from nltk.corpus import stopwords
+def remove_tags(text):
+    tags_to_filter = ['code', 'a']
+    if isinstance(text, six.string_types):
         text = text.encode('utf8')
-    # the following symbols will be replaced with white space
-    symbols = [',','.',':',';','+','=','"','/']
-    for symbol in symbols:
-        text = text.replace(symbol,' ')
-    output = ""
-    for word in text.split():
-        if not word.lower() in stop_w:
-            output += word + ' '
-    return output
-
-
-def filter_html_tags(text):
-    # the following tags and their content will be removed, for example <a> tag will remove any html links
-    tags_to_filter = ['code','a']
-    if isinstance(text, unicode):
-        text = text.encode('utf8')
-    soup = BeautifulSoup(text)
+    soup = BeautifulSoup(text, features='lxml')
     for tag_to_filter in tags_to_filter:
         text_to_remove = soup.findAll(tag_to_filter)
         [tag.extract() for tag in text_to_remove]
     return soup.get_text()
 
+def rem_body_space(text):
+    text = re.sub("\'", "", text)
+    text = re.sub("[^a-zA-Z]", " ", text)
+    text = ' '.join(text.split())
+    text = text.lower()
+    return text
 
-def preprocess_csv(filename, word_set_to_filter=None):
-    if word_set_to_filter is None:
-        word_set_to_filter = set()
-    print 'pre processing '+filename+'...'
-    train_file_object = csv.reader(open('..'+os.path.sep+'csv'+os.path.sep+filename, 'rb'))
-    header = train_file_object.next()
-    preprocessed_file = csv.writer(open('..'+os.path.sep+'csv'+os.path.sep+'preprocess_'+filename, "wb"),quoting=csv.QUOTE_NONNUMERIC)
-    preprocessed_file.writerow(header)
-    # if the csv file is the test data
-    if len(header)==3
-        for row in train_file_object:
-            preprocessed_file.writerow([int(row[0]), filter_common_words(row[1],word_set_to_filter), filter_common_words(filter_html_tags(row[2]),word_set_to_filter)])
-    # if the csv file is the train data
-    if len(header)==4:
-        for row in train_file_object:
-            preprocessed_file.writerow([int(row[0]), filter_common_words(row[1],word_set_to_filter), filter_common_words(filter_html_tags(row[2]),word_set_to_filter),row[3]])
-    print 'finished'
+def freq_words(x, terms=30):
+    all_words = [text for text in x]
+    fdist = nltk.FreqDist(all_words)
+    words_df = pd.DataFrame({'word': list(fdist.keys()), 'count': list(fdist.values())})
+    d = words_df.nlargest(columns="count", n=terms)
 
 
-def get_words(filename):
-    english_words_file = csv.reader(open('..'+os.path.sep+'csv'+os.path.sep+filename, 'rb'))
-    stop_w = set()
-    for row in english_words_file:
-        for word in row:
-            stop_w.add(word.lower())
-    return stop_w
-
-
-if __name__ == '__main__':
-    main()
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
+    no_stopwords_text = [w for w in text.split() if not w in stop_words]
+    return ' '.join(no_stopwords_text)
